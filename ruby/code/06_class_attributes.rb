9e4d9d28635326f94d15ba9f2fa69eddd77766f3
g16her <g16her@hotmail.de>
2015-09-24 09:14:24 +0200

    Update

--- ruby/code/06_class_attributes.rb
+++ ruby/code/06_class_attributes.rb
@@ -3,49 +3,55 @@
 # c. die Eigenschaft name soll sowohl lesbar, als auch schreibbar sein
 # d. erzeugen Sie ein virtuelles Attribut score, das den Wert von health addiert mit der Buchstabenlaenge des name -Attributes
 # e. aendern Sie die to_s - Methode, zur Ausgabe des Score
-
-
 class Player
 
-		attr_reader :health
-		attr_accessor :name
-
-def initialize(pname,phealth=100)
-	@name = pname
-	@health = phealth
-end
-def to_s
-		"ich bin #{@name} mit einem wert von #{@health}"
+	attr_reader :health
+	attr_accessor :name
+	
+	def initialize(name, health = 100)
+		@name = name
+		@health = health
 	end
+
+	def say_hello 
+	 	"Ich bin #{@name} mit einem Wert von #{@health}" 
+	end
+
 	def blam
-		@health -=10
-		puts "#{@name} wurde geblamt"
+		@health -= 10
+		puts "#{@name} got blamed"
 	end
 
 	def w00t
-		@health +=10
-		puts "#{@name} wurde gew00t: #{@health}"
+		@health += 10
+		puts "#{@name} got w00ted"
 	end
 
-	def score
+	def score 		#Virtuelles Attribut
 		@health + @name.length
 	end
 
-
-
-player1 = Player.new('Theo', 5)
-player2 = Player.new('Peter')
-# puts player1.name
-# puts player1.health
-# player1.name ="Willinald"
-# puts player1.name
-# puts player1.score
-players =[player1, player2]
-players.each do | var |
-	puts var 
+	def to_s
+		"Your score is #{score}"
+	end
 end
 
-players.size.times do |i|
-		puts players[i]
+player1 = Player.new('Ralf', 90)
+player2 = Player.new('Jürgen')
+players = player1.say_hello, player2.say_hello
+puts player1.health
+puts player1
+puts player2
+
+# h. packen Sie Ihre drei Player aus dem Projekt in ein Array
+playerss = [player1, player2]
+# i. Geben Sie Player mit einer Iteration über das Array aus. Beginnen Sie mit einer Zeile, in der sie die Anzahl der Mitspieler notieren
+playerss.each do |var|
+	var.blam
+	puts var
 end
 
+playerss.size.times do |i|
+	puts i.to_s
+	puts playerss[i]
+end
