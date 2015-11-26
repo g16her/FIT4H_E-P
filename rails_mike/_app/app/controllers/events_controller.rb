37405bd6cc5aee5985d7b3c6ee4432b2545d1433
g16her <g16her@hotmail.de>
2015-11-26 12:29:02 +0100

    Update

--- rails_mike/_app/app/controllers/events_controller.rb
+++
@@ -1,9 +0,0 @@
-class EventsController < ApplicationController
-	def index
-		@events = Event.all
-	end
-
-	def show
-		@event = Event.find(params[:id])
-	end
-end
--- rails_mike/studio_games/studio_games/app/views/players/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @player, :id, :title, :name, :health, :created_at, :updated_at
