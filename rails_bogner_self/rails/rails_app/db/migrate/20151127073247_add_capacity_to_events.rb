cb89e1744f1cca31c7f48a9c13158dc7e75507ab
g16her <g16her@hotmail.de>
2015-12-18 09:35:02 +0100

    Update

--- rails_bogner_self/rails/rails_app/db/migrate/20151127073247_add_capacity_to_events.rb
+++
@@ -1,7 +0,0 @@
-class AddCapacityToEvents < ActiveRecord::Migration
-  def change
-    add_column :events, :image, :string
-    add_column :events, :file, :string
-    add_column :events, :capacity, :integer
-  end
-end
